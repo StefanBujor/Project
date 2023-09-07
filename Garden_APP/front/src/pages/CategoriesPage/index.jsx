e3fb4ba8a849219b0bd06ed197abef6585d616ba
StefanBujor <man644201@gmail.com>
2023-09-08 00:55:44 +0200

    project full

--- Garden_APP/front/src/pages/CategoriesPage/index.jsx
+++ Garden_APP/front/src/pages/CategoriesPage/index.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCategories } from '../../requests/categories_req';
 import CategoriesContainer from '../../components/CategoriesContainer'
+import s from './index.module.css'
 
 export default function CategoriesPage() {
   
@@ -13,7 +14,7 @@ export default function CategoriesPage() {
   const categories_state = useSelector(state => state.allcategories);
 
   return (
-    <div>
+    <div className={s.categories_container}>
       <h2>Categories</h2>
       <CategoriesContainer categories={categories_state}/>
     </div>
